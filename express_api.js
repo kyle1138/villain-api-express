957b45c9f2ac0fe01e4138935e777537cfc0f603
Kyle Hogan <kylehogan@Kyles-MacBook-Air.local>
2015-01-20 22:39:55 -0500

    no frills site made

---
+++ express_api.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var request = require('request')
+var app = express();
+
+
+var rander = function(arr){
+  index = Math.floor(Math.random() * arr.length);
+  console.log(index);
+  console.log(arr[index-1]);
+  return arr[index];
+
+
+}
+
+
+app.get("/" , function(req,res){
+  res.header("Access-Control-Allow-Origin", "*");
+  console.log("request recieved");
+  var villain = {
+    name:"The " + rander(colors) + " " + rander(names),
+    power:rander(powers),
+    city:rander(cities)
+  };
+  res.send(villain);
+
+})
+
+
+var colors = ["Red" , "Crimson", "Bloody" , "Green" , "Blue","Purple","Black",
+"Yellow","Dark", "Deadly" , "Evil" , "Sinister" , "Vile" , "Phantom" , "Creeping",
+"Hateful","Notorious" , "Monstrous" , "Malevolent" , "Malificent" , "Baleful" , "Vicious" ,
+"Malignant" , "Satanic" , "Murderous"];
+
+var names = ["Rat" , "Blade" , "Killer" , "Dreadnaught" , "Viper" , "Claw" , "Assassin" ,
+"Plague" , "Terror" , "Reaver" , "Menace" , "Mortimer" , "Creeper"
+, "Monster" , "Reaper"];
+
+var powers = ["Lasers" , "Poison Gas" , "Claws" , "Telekinetic Powers" , "Supernatural Karate",
+"Firey Breath" , "Military Ordnance" , "Super Strength" , "Criminal Syndicate" ,
+"Bombs" , "Mind Control" , "Criminal Genius" , "Infectious Disease" , "Killer" , "Maniac"
+, "Skull", "Ghost"];
+
+var cities = ["Speonk" , "Des Moines" , "Paramus" , "Ronkonkoma" , "Akron" ,
+"Yaphank" , "New Rochelle", "Reno" , "Hackensack"];
+
+app.listen(3000);
